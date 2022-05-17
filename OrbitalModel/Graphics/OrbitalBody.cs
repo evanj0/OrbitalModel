b3265e3e7c4160ba1e024307ae2b375b7cf2d6fe
Evan Jones <48502664+evanj0@users.noreply.github.com>
2022-05-16 18:46:16 -0700

    adding way to initialize bodies with orbital elements

---
+++ OrbitalModel/Graphics/OrbitalBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalModel.Graphics;
+
+public class OrbitalBody
+{
+    public float Inclination { get; set; } // i
+    public float LongitudeOfAscendingNode { get; set; } // Ω
+    public float ArgumentOfPeriapsis { get; set; } // ω
+    public float PericenterEpoch { get; set; }
+    public float PericenterDistance { get; set; }
+    public float PericenterVelocity { get; set; }
+}
