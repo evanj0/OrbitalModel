b9bbbb0494dac0c8739fa7bac75d5723844f7847
Evan Jones <48502664+evanj0@users.noreply.github.com>
2022-05-18 09:59:17 -0700

    Update OrbitalBody.cs

--- OrbitalModel/Graphics/OrbitalBody.cs
+++ OrbitalModel/Graphics/OrbitalBody.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK.Mathematics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,56 @@ namespace OrbitalModel.Graphics;
 
 public class OrbitalBody
 {
-    public float Inclination { get; set; } // i
-    public float LongitudeOfAscendingNode { get; set; } // Ω
-    public float ArgumentOfPeriapsis { get; set; } // ω
-    public float PericenterEpoch { get; set; }
-    public float PericenterDistance { get; set; }
-    public float PericenterVelocity { get; set; }
+    public double Inclination { get; set; } // i
+    public double LongitudeOfAscendingNode { get; set; } // Ω
+    public double ArgumentOfPeriapsis { get; set; } // ω
+    public double PericenterEpoch { get; set; }
+    public double PericenterDistance { get; set; }
+    public double PericenterVelocity { get; set; }
+    public double Period { get; set; }
+
+    public double MeanAnomaly(double t)
+    {
+        return 2.0 * Math.PI / Period * (t - PericenterEpoch);
+    }
+
+    public double EccentricAnomaly(double meanAnomaly, double eccentricity)
+    {
+        Func<double, double> f = E => E + (eccentricity * Math.Sin(E)) - meanAnomaly;
+        Func<double, double> fPrime = E => 1 + eccentricity * Math.Cos(E);
+        var E = NewtonIterations(f, fPrime, meanAnomaly, 100);
+        return E;
+    }
+
+    public double TrueAnomaly(double eccentricAnomaly, double e)
+    {
+        return 2 * Math.Atan(Math.Sqrt((1.0 + e) / (1.0 - e)) * Math.Tan(eccentricAnomaly / 2.0));
+    }
+
+    public double Radius(double trueAnomaly)
+    {
+        return 1;
+    }
+
+    public double SinExpansion(double x, int nMax)
+    {
+        var sum = 0.0;
+        for (var n = 0; n < nMax; n++)
+        {
+            sum += Math.Pow(-1, n) * Math.Pow(x, (2 * n) + 1) / MathHelper.Factorial((2 * n) + 1);
+        }
+        return sum;
+    }
+
+    public double NewtonIterations(Func<double, double> f, Func<double, double> fPrime, double x0, int iterations)
+    {
+        var xn = x0;
+        for (var i = 0; i < iterations; i++)
+        {
+            var xIntercept = f(xn) / fPrime(xn) + xn;
+            xn = xIntercept;
+        }
+        return xn;
+    }
+
 }
